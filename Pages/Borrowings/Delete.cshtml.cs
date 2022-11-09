019bb5933dc7c3ae4cbdf5c21ca270491b094b7d
Emanuela Fodor <64927697+EmmaFodor@users.noreply.github.com>
2022-11-09 21:16:20 +0200

    Update Delete.cshtml.cs

--- Pages/Borrowings/Delete.cshtml.cs
+++ Pages/Borrowings/Delete.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,19 +7,21 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Fodor_Emanuela_lab2.Data;
 using Fodor_Emanuela_lab2.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json.Linq;
+using System.Xml.Linq;
 
 namespace Fodor_Emanuela_lab2.Pages.Borrowings
 {
-    public class DeleteModel : PageModel
+    public class DetailsModel : PageModel
     {
         private readonly Fodor_Emanuela_lab2.Data.Fodor_Emanuela_lab2Context _context;
 
-        public DeleteModel(Fodor_Emanuela_lab2.Data.Fodor_Emanuela_lab2Context context)
+        public DetailsModel(Fodor_Emanuela_lab2.Data.Fodor_Emanuela_lab2Context context)
         {
             _context = context;
         }
 
-        [BindProperty]
       public Borrowing Borrowing { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
@@ -29,8 +31,7 @@ namespace Fodor_Emanuela_lab2.Pages.Borrowings
                 return NotFound();
             }
 
-            var borrowing = await _context.Borrowing.FirstOrDefaultAsync(m => m.ID == id);
-
+            var borrowing = await _context.Borrowing.Include("Member").Include("Book").FirstOrDefaultAsync(m => m.ID == id);
             if (borrowing == null)
             {
                 return NotFound();
@@ -39,25 +40,11 @@ namespace Fodor_Emanuela_lab2.Pages.Borrowings
             {
                 Borrowing = borrowing;
             }
+
+
+            ViewData["MemberID"] = new SelectList(_context.Member, "ID", "FullName");
+            ViewData["BookID"] = new SelectList(_context.Book, "ID", "Title");
             return Page();
-        }
-
-        public async Task<IActionResult> OnPostAsync(int? id)
-        {
-            if (id == null || _context.Borrowing == null)
-            {
-                return NotFound();
-            }
-            var borrowing = await _context.Borrowing.FindAsync(id);
-
-            if (borrowing != null)
-            {
-                Borrowing = borrowing;
-                _context.Borrowing.Remove(Borrowing);
-                await _context.SaveChangesAsync();
-            }
-
-            return RedirectToPage("./Index");
         }
     }
 }
