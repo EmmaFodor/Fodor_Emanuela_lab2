b8067140715cf33f7710298e881bc0416cfbb061
Emanuela Fodor <64927697+EmmaFodor@users.noreply.github.com>
2022-11-09 21:20:05 +0200

    Update Delete.cshtml.cs

--- Pages/Borrowings/Delete.cshtml.cs
+++ Pages/Borrowings/Delete.cshtml.cs
@@ -7,21 +7,19 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Fodor_Emanuela_lab2.Data;
 using Fodor_Emanuela_lab2.Models;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Newtonsoft.Json.Linq;
-using System.Xml.Linq;
 
 namespace Fodor_Emanuela_lab2.Pages.Borrowings
 {
-    public class DetailsModel : PageModel
+    public class DeleteModel : PageModel
     {
         private readonly Fodor_Emanuela_lab2.Data.Fodor_Emanuela_lab2Context _context;
 
-        public DetailsModel(Fodor_Emanuela_lab2.Data.Fodor_Emanuela_lab2Context context)
+        public DeleteModel(Fodor_Emanuela_lab2.Data.Fodor_Emanuela_lab2Context context)
         {
             _context = context;
         }
 
+        [BindProperty]
       public Borrowing Borrowing { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
@@ -31,7 +29,8 @@ namespace Fodor_Emanuela_lab2.Pages.Borrowings
                 return NotFound();
             }
 
-            var borrowing = await _context.Borrowing.Include("Member").Include("Book").FirstOrDefaultAsync(m => m.ID == id);
+            var borrowing = await _context.Borrowing.FirstOrDefaultAsync(m => m.ID == id);
+
             if (borrowing == null)
             {
                 return NotFound();
@@ -40,11 +39,25 @@ namespace Fodor_Emanuela_lab2.Pages.Borrowings
             {
                 Borrowing = borrowing;
             }
-
-
-            ViewData["MemberID"] = new SelectList(_context.Member, "ID", "FullName");
-            ViewData["BookID"] = new SelectList(_context.Book, "ID", "Title");
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Borrowing == null)
+            {
+                return NotFound();
+            }
+            var borrowing = await _context.Borrowing.FindAsync(id);
+
+            if (borrowing != null)
+            {
+                Borrowing = borrowing;
+                _context.Borrowing.Remove(Borrowing);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
         }
     }
 }
